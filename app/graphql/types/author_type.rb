6d7a964217ea9dfb5c9aa210fffc632c09faa39d
muh riansyah <mriansyah93@gmail.com>
2019-09-05 06:31:08 +0700

    change field null-status from true to false

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -3,8 +3,8 @@ module Types
 	field :id, ID, null: false
 	field :email, String, null: false
   	field :name, String, null: false 	
-  	field :books, [Types::BookType], null: true
-    field :number_of_books, Integer, null: true
+  	field :books, [Types::BookType], null: false
+    field :number_of_books, Integer, null: false
 
     def number_of_books
 		object.books.size
